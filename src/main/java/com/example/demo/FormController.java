4825778798e99a07b49e2717d48aa3a727028177
nikhil sharma <nikhilsharma599@gmail.com>
2022-02-26 15:22:54 +0530

    Adding jasper dependecy - Basic template for web Applications

---
+++ src/main/java/com/example/demo/FormController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FormController {
+	@RequestMapping("/")
+	public String edureka() {
+		return "edureka.jsp";
+	}
+}
