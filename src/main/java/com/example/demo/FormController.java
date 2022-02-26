1333c524d86279b27313a9716882be40741e644c
nikhil sharma <nikhilsharma599@gmail.com>
2022-02-26 19:02:56 +0530

    Working of jsp files - configuring formController and sumFormApplication.java

--- src/main/java/com/example/demo/FormController.java
+++ src/main/java/com/example/demo/FormController.java
@@ -1,12 +1,25 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
-	@RequestMapping("/")
+	@GetMapping("/edureka")
 	public String edureka() {
 		return "edureka";
+	}
+	@PostMapping("/details")
+	public String viewdetails(@RequestParam("cid") String cid,
+			@RequestParam("cname") String cname,
+			@RequestParam("cemail") String cemail, ModelMap mp) {
+		mp.put("cid", cid);
+		mp.put("cname", cname);
+		mp.put("cemail", cemail);
+		return "viewCustomer";
+		
 	}
 }
--- src/main/java/com/example/demo/SumFormApplication.java
+++ src/main/java/com/example/demo/SumFormApplication.java
@@ -2,9 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan
 @SpringBootApplication
-public class SumFormApplication {
+public class SumFormApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SumFormApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SumFormApplication.class, args);
