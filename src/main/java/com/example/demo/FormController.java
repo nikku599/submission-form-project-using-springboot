d9e42cc1a353ec09c38ee0532211ae956725fd98
nikhil sharma <nikhilsharma599@gmail.com>
2022-02-28 10:35:34 +0530

    Working RESTAPI with postman over /customers

--- src/main/java/com/example/demo/FormController.java
+++ src/main/java/com/example/demo/FormController.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -65,6 +68,25 @@ public class FormController {
 	@ResponseBody
 	public Optional<Customers> getCustomer(@PathVariable("cid") int cid) {
 		return repo.findById(cid);
+	}
+	
+	@PostMapping("/customers")
+	public Customers getCustomer2(@RequestBody Customers customer) {
+		repo.save(customer);
+		return customer;
+	}
+	
+	@DeleteMapping("/customers/{cid}")
+	public Customers getCustomer3(@PathVariable("cid") int cid) {
+		Customers cust = repo.getOne(cid);
+		repo.delete(cust);
+		return cust;
+	}
+	
+	@PutMapping(path="/customers", consumes="application/json")
+	public Customers getCustomer4(@RequestBody Customers customer) {
+		repo.save(customer);
+		return customer;
 	}
 	
 }
